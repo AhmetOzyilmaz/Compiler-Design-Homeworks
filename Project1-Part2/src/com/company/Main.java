a3121e86a0d382ba18e2bbbe49b78ddd2974d917
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-05-01 17:55:45 +0300

    Adding Teminal testing program
    This program take .c file and converting asm program tm_WIN.exe running at command line

--- Project1-Part2/src/com/company/Main.java
+++ Project1-Part2/src/com/company/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        if(args.length != 3)
+        {
+            System.out.print("Usage ./Simulate NewSort.txt\n");
+            System.exit(0);
+        }
+        else if(args.length == 3)
+        {
+            String filename = args[1];
+
+            //Simulate sim(filename,mode);
+           // sim.cpuRun();
+        }
     }
 }
